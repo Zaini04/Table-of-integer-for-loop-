c23b965a182f589c78df1cab3293f8981dd8a0e8
ZAIN IQBAL <124054465+Zaini04@users.noreply.github.com>
2023-02-09 21:58:11 +0500

    Add files via upload

---
+++ table using for-loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int table;
+	cin>>table;
+	for(int i = 0 ; i <= 10 ; ++i){
+		cout<<"Here is my Table "<<table<<" * "<<i << " = "<<table * i<<endl;
+	}
+return 0;
+}
